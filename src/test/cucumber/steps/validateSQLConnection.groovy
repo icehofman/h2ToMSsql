71790ec4aad4d1abde2dd0b0d3fcbb30c23f80f0
Israel Hofman <icehofman@hotmail.com>
2015-05-22 13:36:38 -0300

    Test connection string to validate db.

--- src/test/cucumber/steps/validateSQLConnection.groovy
+++ src/test/cucumber/steps/validateSQLConnection.groovy
@@ -1,13 +1,21 @@
 import static cucumber.api.groovy.EN.*
+import groovy.sql.Sql
+
+def db = [url:'jdbc:jtds:sqlserver://ICEHOFMAN-PC:1433/groovy', user:'groovy', password:'groovy', driver:'net.sourceforge.jtds.jdbc.Driver']
+def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
+def result
 
 Given(~/^database$/) { ->
-
+      db
 }
 
 When(~/^connect$/) { ->
-
+    sql
 }
 
 Then(~/^connection is sucessful$/) { ->
+    if(!sql.connection.isClosed()){
+        sql.close()
+    }
 
 }
