f1e19027f943a16cf8fd8607b934e987e6a7af36
Israel Hofman <icehofman@hotmail.com>
2015-05-26 15:28:55 -0300

    Create a table if connection is successful.

--- src/test/cucumber/steps/validateH2Connection.groovy
+++ src/test/cucumber/steps/validateH2Connection.groovy
@@ -1,14 +1,41 @@
 import static cucumber.api.groovy.EN.*
 import groovy.sql.Sql
 
-Given(~/^database groovy on HTwo$/) { ->
+def db = [url:'jdbc:h2:things', user:'sa', password:'sa', driver:'org.h2.Driver']
 
+def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
+
+Given(~/^database groovy on HTwo$/) { ->
+    db
 }
 
 When(~/^connect to HTwo$/) { ->
-
+    sql
 }
 
 Then(~/^connection is successful  to HTwo$/) { ->
+    def createTbl = '''
+CREATE TABLE things (
+  id INT PRIMARY KEY,
+  thing1 VARCHAR(50),
+  thing2 VARCHAR(100)
+)
+'''
+    sql.execute("DROP TABLE IF EXISTS things")
+    sql.execute(createTbl)
+    sql.execute("INSERT INTO things VALUES(:id, :thing1, :thing2)", [id: 0, thing1: 'I am thing1', thing2: 'I am thing2'])
+    sql.execute("INSERT INTO things VALUES(:id, :thing1, :thing2)", [id: 1, thing1: 'foo', thing2: 'bar'])
+    sql.execute("INSERT INTO things VALUES(:id, :thing1, :thing2)", [id: 2, thing1: 'Alisa', thing2: 'Yeoh'])
 
+    def query = "SELECT * FROM things"
+
+    sql.eachRow(query) { row ->
+        println "$row.id - ${row.thing1}::$row.thing2"
+    }
+
+    if(sql){
+        if(!sql.connection.isClosed()){
+            sql.close()
+        }
+    }
 }
