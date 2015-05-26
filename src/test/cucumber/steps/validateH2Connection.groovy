f47d7ed7626c30962a2e972ab922f9653e4b458b
Israel Hofman <icehofman@hotmail.com>
2015-05-26 17:39:02 -0300

    Refactoring code.

--- src/test/cucumber/steps/validateH2Connection.groovy
+++ src/test/cucumber/steps/validateH2Connection.groovy
@@ -3,39 +3,40 @@ import groovy.sql.Sql
 
 def db = [url:'jdbc:h2:things', user:'sa', password:'sa', driver:'org.h2.Driver']
 
-def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
+def sqlH2 = Sql.newInstance(db.url, db.user, db.password, db.driver)
 
 Given(~/^database groovy on HTwo$/) { ->
     db
 }
 
 When(~/^connect to HTwo$/) { ->
-    sql
+    sqlH2
 }
 
 Then(~/^connection is successful  to HTwo$/) { ->
-    def createTbl = '''
+
+    if(sqlH2){
+        def createTbl = '''
 CREATE TABLE things (
   id INT PRIMARY KEY,
   thing1 VARCHAR(50),
   thing2 VARCHAR(100)
 )
 '''
-    sql.execute("DROP TABLE IF EXISTS things")
-    sql.execute(createTbl)
-    sql.execute("INSERT INTO things VALUES(:id, :thing1, :thing2)", [id: 0, thing1: 'I am thing1', thing2: 'I am thing2'])
-    sql.execute("INSERT INTO things VALUES(:id, :thing1, :thing2)", [id: 1, thing1: 'foo', thing2: 'bar'])
-    sql.execute("INSERT INTO things VALUES(:id, :thing1, :thing2)", [id: 2, thing1: 'Alisa', thing2: 'Yeoh'])
+        sqlH2.execute("DROP TABLE IF EXISTS things")
+        sqlH2.execute(createTbl)
+        sqlH2.execute("INSERT INTO things VALUES(:id, :thing1, :thing2)", [id: 0, thing1: 'I am thing1', thing2: 'I am thing2'])
+        sqlH2.execute("INSERT INTO things VALUES(:id, :thing1, :thing2)", [id: 1, thing1: 'foo', thing2: 'bar'])
+        sqlH2.execute("INSERT INTO things VALUES(:id, :thing1, :thing2)", [id: 2, thing1: 'Alisa', thing2: 'Yeoh'])
 
-    def query = "SELECT * FROM things"
+        def query = "SELECT * FROM things"
 
-    sql.eachRow(query) { row ->
-        println "$row.id - ${row.thing1}::$row.thing2"
-    }
+        sqlH2.eachRow(query) { row ->
+            println "$row.id - ${row.thing1}::$row.thing2"
+        }
 
-    if(sql){
-        if(!sql.connection.isClosed()){
-            sql.close()
+        if(!sqlH2.connection.isClosed()){
+            sqlH2.close()
         }
     }
 }
--- src/test/cucumber/steps/validateSQLConnection.groovy
+++ src/test/cucumber/steps/validateSQLConnection.groovy
@@ -3,13 +3,13 @@ import groovy.sql.Sql
 
 def db = [url:'jdbc:jtds:sqlserver://ICEHOFMAN-PC:1433/groovy', user:'groovy', password:'groovy', driver:'net.sourceforge.jtds.jdbc.Driver']
 
-def sql
+def sqlServer
 
 try {
-    sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
+    sqlServer = Sql.newInstance(db.url, db.user, db.password, db.driver)
 }
 catch (all){
-
+    println "#### Error SqlServer ####"
 }
 
 Given(~/^database groovy on SqlServer$/) { ->
@@ -17,13 +17,41 @@ Given(~/^database groovy on SqlServer$/) { ->
 }
 
 When(~/^connect to SqlServer$/) { ->
-    sql
+    sqlServer
 }
 
 Then(~/^connection is successful to SqlServer$/) { ->
-    if(sql){
-        if(!sql.connection.isClosed()){
-            sql.close()
+
+    if(sqlServer){
+        def createTbl = '''
+CREATE TABLE things (
+  id INT PRIMARY KEY,
+  thing1 VARCHAR(50),
+  thing2 VARCHAR(100)
+)
+'''
+
+def dropTable = '''
+IF  EXISTS (
+SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'things') AND type in (N'U')
+)
+DROP TABLE things
+
+'''
+        sqlServer.execute(dropTable)
+        sqlServer.execute(createTbl)
+        sqlServer.execute("INSERT INTO things VALUES(:id, :thing1, :thing2)", [id: 0, thing1: 'I am thing1', thing2: 'I am thing2'])
+        sqlServer.execute("INSERT INTO things VALUES(:id, :thing1, :thing2)", [id: 1, thing1: 'foo', thing2: 'bar'])
+        sqlServer.execute("INSERT INTO things VALUES(:id, :thing1, :thing2)", [id: 2, thing1: 'Alisa', thing2: 'Yeoh'])
+        sqlServer.execute("INSERT INTO things VALUES(:id, :thing1, :thing2)", [id: 3, thing1: 'Israel', thing2: 'Hofman'])
+
+        def query = "SELECT * FROM things"
+
+        sqlServer.eachRow(query) { row ->
+            println "$row.id - ${row.thing1}::$row.thing2"
+        }
+        if(!sqlServer.connection.isClosed()){
+            sqlServer.close()
         }
     }
 }
