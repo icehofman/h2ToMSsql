dfa215aada1a6fc88c8b83e1528dcfcf2af77067
Israel Hofman <icehofman@hotmail.com>
2015-05-27 16:27:22 -0300

    Create feature and steps for Gstorm on H2

---
+++ src/test/cucumber/steps/validateGstorm.groovy
@@ -0,0 +1,13 @@
+import static cucumber.api.groovy.EN.*
+
+Given(~/^the class 'Person'$/) { ->
+
+}
+
+When(~/^using the Gstorm on Htwo$/) { ->
+
+}
+
+Then(~/^must create a new record on Htwo$/) { ->
+
+}
