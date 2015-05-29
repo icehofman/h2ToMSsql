d0d284f39321294a9da76f91c4003c699492d3c5
Israel Hofman <icehofman@hotmail.com>
2015-05-29 16:17:54 -0300

    Create tests for dataset.

--- src/test/cucumber/steps/validateDatSet.groovy
+++ src/test/cucumber/steps/validateDatSet.groovy
@@ -1,17 +1,61 @@
+import groovy.sql.*
 import static cucumber.api.groovy.EN.*
 
-Given(~/^An existing database 'Groovy'$/) { ->
+def db = [url:'jdbc:jtds:sqlserver://ICEHOFMAN-PC:1433/groovy', user:'groovy', password:'groovy', driver:'net.sourceforge.jtds.jdbc.Driver']
 
+def sqlServer
+
+def setOfSimpleOrder
+
+try {
+    sqlServer = Sql.newInstance(db.url, db.user, db.password, db.driver)
+    setOfSimpleOrder = new DataSet(sqlServer, 'SimpleOrders')
+}
+catch (all){
+    println "#### Error SqlServer Transaction####"
 }
 
-Given(~/^its respective table 'SimpleOrder'$/) { ->
+Given(~/^An existing database 'Groovy'$/) { ->
+    assert 'jdbc:jtds:sqlserver://ICEHOFMAN-PC:1433/groovy' == db.url
+}
 
+Given(~/^its respective table 'SimpleOrders'$/) { ->
+    if(sqlServer) {
+        def createTbl = '''
+        CREATE TABLE SimpleOrders (
+          id uniqueidentifier PRIMARY KEY NOT NULL
+          DEFAULT newid(),
+          OrderNumber NVARCHAR(50),
+          OrderDesc NVARCHAR(100)
+        )
+        '''
+
+        def dropTable = '''
+        IF  EXISTS (
+        SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SimpleOrders') AND type in (N'U')
+        )
+        DROP TABLE SimpleOrders
+        '''
+
+        sqlServer.execute(dropTable)
+
+        sqlServer.execute(createTbl)
+
+        def result = sqlServer.rows("SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'SimpleOrders') AND type in (N'U')")
+
+        assert 1 == result.size()
+    }
 }
 
 When(~/^creating a record with dataset in SqlServer$/) { ->
-
+    if(sqlServer) {
+        setOfSimpleOrder.add(orderNumber: '000abc', orderDesc: 'Test01')
+    }
 }
 
 Then(~/^validate the new record with dataset in SqlServer$/) { ->
-
+    if(sqlServer) {
+        def rows = setOfSimpleOrder.rows()
+        assert 1 == rows.size()
+    }
 }
--- src/test/cucumber/validateDataSet.feature
+++ src/test/cucumber/validateDataSet.feature
@@ -1,7 +1,7 @@
 Feature: Validate DataSet
   Background: Database and table
     Given An existing database 'Groovy'
-    And its respective table 'SimpleOrder'
+    And its respective table 'SimpleOrders'
 
   Scenario: Test the dataset in SqlServer
     When creating a record with dataset in SqlServer
